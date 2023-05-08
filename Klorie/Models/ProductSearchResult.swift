9dea726e13f220df96ec4751e453cf08e9b8d523
Timea Bartha <timea.bartha.93@gmail.com>
2023-05-08 17:01:51 +0200

    Updated Product Models
    The previous model was faulty and incomplete, added enums.

--- Klorie/Models/ProductSearchResult.swift
+++ Klorie/Models/ProductSearchResult.swift
@@ -6,21 +6,43 @@
 //
 
 import SwiftUI
+// To parse the JSON, add this file to your project and do:
+//
+//   let productSearch = try? JSONDecoder().decode(ProductSearch.self, from: jsonData)
 
+import Foundation
+
+// MARK: - ProductSearch
 struct ProductSearch: Codable {
-    var count: Int
-    var page: Int
-    var page_count: Int
-    var page_cize: Int
+    var count, page, pageCount, pageSize: Int
     var products: [ProductSearchItem]
-    
+    var skip: Int
+
+    enum CodingKeys: String, CodingKey {
+        case count, page
+        case pageCount = "page_count"
+        case pageSize = "page_size"
+        case products, skip
+    }
 }
 
+// MARK: - ProductSearchItem
 struct ProductSearchItem: Codable, Identifiable {
     var brands: String
-    var categories: String
+    var categories: String?
     var id: String
-    var nutrition_data: String
-    var serving_size: String
-    
+    var code: String
+    var nutritionData: NutritionData?
+    var servingSize, productName: String?
+
+    enum CodingKeys: String, CodingKey {
+        case brands, categories, id, code
+        case nutritionData = "nutrition_data"
+        case servingSize = "serving_size"
+        case productName = "product_name"
+    }
+}
+
+enum NutritionData: String, Codable {
+    case on = "on"
 }
--- Klorie/Models/SingleProduct.swift
+++ Klorie/Models/SingleProduct.swift
@@ -3,45 +3,89 @@
 //  Klorie
 //
 //  Created by Timea Bartha on 3/5/23.
+/// To parse the JSON, add this file to your project and do:
 //
+//   let productresponse = try? JSONDecoder().decode(ProductResponse.self, from: jsonData)
+
 
 import SwiftUI
+// MARK: - Product Response
 
 struct ProductResponse: Codable {
     var code: String
     var status: Int
     var product: [SingleProduct]
-    var status_vebrose: String
+    var statusVerbose: String
+    
+    enum CodingKeys: String, CodingKey {
+            case code, product, status
+            case statusVerbose = "status_verbose"
+        }
 
 }
+
+// MARK: - Single Product
 
 struct SingleProduct: Codable, Identifiable {
     var id: String
     var keywords: [String]
     var nutriments: [SingleProductNutriments]
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case keywords = "_keywords"
+        case nutriments
+    }
 }
+
+// MARK: - Single Product Nutriments
 
 struct SingleProductNutriments: Codable {
-    var carbohydrates100g: Double
-    var fat_100g: Double
-    var fiber_100g : Double
-    var proteins_100g : Double
-    var salt_100g : Double
-    var sodium_100g : Double
-    var sugars_100g : Double
+    var carbohydrates, carbohydrates100G, carbohydratesServing: Double
+    var carbohydratesUnit: String
+    let fat, fat100G, fatServing: Int
+    var fatUnit: String
+    var fiber, fiber100G, fiberServing: Int
+    var fiberUnit: String
+    var proteins, proteins100G, proteinsServing: Int
+    var proteinsUnit: String
+    var salt, salt100G, saltServing: Double
+    var saltUnit: String
+    var sodium, sodium100G, sodiumServing: Double
+    var sodiumUnit: String
+    var sugars, sugars100G, sugarsServing: Double
+    var sugarsUnit: String
     
+    enum CodingKeys: String, CodingKey {
+        case carbohydrates
+        case carbohydrates100G = "carbohydrates_100g"
+        case carbohydratesServing = "carbohydrates_serving"
+        case carbohydratesUnit = "carbohydrates_unit"
+        case fat
+        case fat100G = "fat_100g"
+        case fatServing = "fat_serving"
+        case fatUnit = "fat_unit"
+        case fiber
+        case fiber100G = "fiber_100g"
+        case fiberServing = "fiber_serving"
+        case fiberUnit = "fiber_unit"
+        case proteins
+        case proteins100G = "proteins_100g"
+        case proteinsServing = "proteins_serving"
+        case proteinsUnit = "proteins_unit"
+        case salt
+        case salt100G = "salt_100g"
+        case saltServing = "salt_serving"
+        case saltUnit = "salt_unit"
+        case sodium
+        case sodium100G = "sodium_100g"
+        case sodiumServing = "sodium_serving"
+        case sodiumUnit = "sodium_unit"
+        case sugars
+        case sugars100G = "sugars_100g"
+        case sugarsServing = "sugars_serving"
+        case sugarsUnit = "sugars_unit"
+    }
     
 }
-//var SingleProductNutriments = [
- //   "carbohydrates100g": 6.9,
-//    "fat_100g": 0,
-//   "fiber_100g": 0,
- //   "proteins_100g": 0,
-//    "salt_100g": 0.01,
-//    "saturated-fat_100g": 0,
-//    "sodium_100g": 0.004,
-//    "sugars_100g": 6.8,
-//    "energy-kcal_100g": 28,
-
-//]
 
