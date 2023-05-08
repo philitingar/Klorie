d527e337a218f61fd3909cbb4271c77c693bf81d
Timea Bartha <timea.bartha.93@gmail.com>
2023-05-08 17:05:03 +0200

    Created the barcodeScannerView
    Nothing added in yet

---
+++ Klorie/Views/BarcodeScannerView.swift
@@ -0,0 +1,20 @@
+//
+//  Barcode.swift
+//  Klorie
+//
+//  Created by Timea Bartha on 8/5/23.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text("This is trhe barcode scanner")
+    }
+}
+
+struct BarcodeScannetView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
