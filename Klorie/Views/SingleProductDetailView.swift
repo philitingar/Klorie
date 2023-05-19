2002f1ce161e8120bf2e1647d763b17f7bbfbb33
Timea Bartha <timea.bartha.93@gmail.com>
2023-05-19 15:04:50 +0200

    Made the nutriscore change colors depending on score.

--- Klorie/Views/SingleProductDetailView.swift
+++ Klorie/Views/SingleProductDetailView.swift
@@ -19,12 +19,17 @@ struct SingleProductDetailView: View {
     @State var fat = 0.0
     @State var protein = 0.0
     @State var calories = 0.0
-    @State private var isPressed = false
-
+    
+    @State private var isNutriscoreValue = false
+    @State var nutriScore = "a"
+    @State var scoreColor: Color = .secondary
+    
+    
     
     var body: some View {
         List {
             if let product = singleProduct {
+                // MARK: Product
                 Section(header: Text("Product")) {
                     VStack(alignment: .leading) {
                         Text(product.code)
@@ -33,6 +38,7 @@ struct SingleProductDetailView: View {
                         Text(product.data.productName)
                     }
                 }
+                // MARK: Calories
                 Section(header: Text("Calories")) {
                     TextField("Enter amount(g)", value: $servings, format: .number)
                         .keyboardType(.decimalPad)
@@ -40,7 +46,7 @@ struct SingleProductDetailView: View {
                             self.fat = (product.data.nutriments.fat100G/100) * newValue
                             self.protein = (product.data.nutriments.proteins100G/100) * newValue
                             self.carbs = (product.data.nutriments.carbohydrates100G/100) * newValue
-
+                            
                         })
                     
                     HStack {
@@ -53,6 +59,7 @@ struct SingleProductDetailView: View {
                     }
                     
                 }
+                // MARK: Nutrients
                 Section(header: Text("Nutrients")) {
                     VStack {
                         HStack {
@@ -79,69 +86,137 @@ struct SingleProductDetailView: View {
                     }
                     
                 }
-                Section(header: Text("Nutriscore")) {
-                    ZStack(alignment: .center ){
-                        HStack(alignment: .center) {
-                            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                .fill(.cyan)
-                                .frame(width: 40, height: 50)
-                                .padding(-3)
-                                .overlay(
-                                    Text("A")
-                                        .font(.largeTitle)
-                                        .bold()
-                                )
-                                .scaleEffect(isPressed ? 0.5 : 1.0)
-                                .animation(.easeInOut, value: isPressed)
+                // MARK: Nutriscore
+                if self.nutriScore == "" {
+                    
+                } else {
+                    Section(header: Text("Nutriscore")) {
+                        ZStack(alignment: .center ){
+                            HStack(alignment: .center) {
+                                if self.nutriScore == "a" {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.green)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("A")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                    
+                                } else {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.secondary)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("A")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                }
                                 
+                                if self.nutriScore == "b" {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.mint)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("B")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                    
+                                } else {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.secondary)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("B")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                }
+                                if self.nutriScore == "c" {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.green)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("B")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                    
+                                } else {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.secondary)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("B")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                }
+                                if self.nutriScore == "d" {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.green)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("D")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                    
+                                } else {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.secondary)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("D")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                }
+                                if self.nutriScore == "e" {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.red)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("E")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                    
+                                } else {
+                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                        .fill(.secondary)
+                                        .frame(width: 40, height: 45)
+                                        .padding(-3)
+                                        .overlay(
+                                            Text("E")
+                                                .font(.largeTitle)
+                                                .bold()
+                                        )
+                                }
                                 
-                            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                .fill(.mint)
-                                .frame(width: 40, height: 50)
-                                .padding(-3)
-                                .overlay(
-                                    Text("B")
-                                        .font(.largeTitle)
-                                        .bold()
-                                )
-                            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                .fill(.yellow)
-                                .frame(width: 40, height: 50)
-                                .padding(-3)
-                                .overlay(
-                                    Text("C")
-                                        .font(.largeTitle)
-                                        .bold()
-                                )
-                            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                .fill(.orange)
-                                .frame(width: 40, height: 50)
-                                .padding(-3)
-                                .overlay(
-                                    Text("D")
-                                        .font(.largeTitle)
-                                        .bold()
-                                )
-                            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                .fill(.red)
-                                .frame(width: 40, height: 50)
-                                .padding(-3)
-                                .overlay(
-                                    Text("E")
-                                        .font(.largeTitle)
-                                        .bold()
-                                )
-                            
-                        }.padding(1)
+                            }.padding(1)
+                        }
+                        
                     }
                 }
             }
         }.onAppear(perform: loadData)
-        .onTapGesture {
-            self.hideKeyboard(focus: true)
-        }
+            .onTapGesture {
+                self.hideKeyboard(focus: true)
+            }
     }
-    
+    // MARK: LoadData
     func loadData() {
         self.singleProduct = nil
         print("looking server call")
@@ -155,15 +230,31 @@ struct SingleProductDetailView: View {
             self.protein = loadedProduct.data.nutriments.proteins100G
             self.carbs = loadedProduct.data.nutriments.carbohydrates100G
             self.calories = loadedProduct.data.nutriments.energyKcal100g
-
-
+            self.nutriScore = loadedProduct.data.nutriscoreData.grade
+            
+            
         }.resume()
     }
-    
+    // MARK: Hide Keyboard
     func hideKeyboard(focus: Bool) {
         if focus {
             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
+    }
+    func nutriScoreValueChanged() {
+        if nutriScore == "a" {
+            isNutriscoreValue = true
+        } else if nutriScore == "b" {
+            isNutriscoreValue = true
+        }  else if nutriScore == "c" {
+            isNutriscoreValue = true
+        } else if nutriScore == "d" {
+            isNutriscoreValue = true
+        } else if nutriScore == "e" {
+            isNutriscoreValue = true
+        }
+        
+        
     }
 }
 
@@ -171,7 +262,7 @@ struct SingleProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
         let searchResult = Bundle.main.decode(ProductSearch.self, from: "SearchResults.json")
         let selectedProduct = searchResult.products.first
-
+        
         SingleProductDetailView(selectedProduct: selectedProduct!)
     }
 }
