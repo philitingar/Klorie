987931ea01dea4e1f19a7580b50bcb3afc1b70e0
Timea Bartha <timea.bartha.93@gmail.com>
2023-06-29 17:38:46 +0200

    Added a data controller
    To work with CoreData

---
+++ Klorie/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  Klorie
+//
+//  Created by Timea Bartha on 29/6/23.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Klorie")
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to lead: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+
