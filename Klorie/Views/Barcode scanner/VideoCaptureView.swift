dade49a2d57557f3056f38af93c2217f515315bf
Timea Bartha <timea.bartha.93@gmail.com>
2023-08-17 17:34:51 +0200

    Added VideoCapture view

---
+++ Klorie/Views/Barcode scanner/VideoCaptureView.swift
@@ -0,0 +1,28 @@
+//
+//  VideoCaptureView.swift
+//  Klorie
+//
+//  Created by Timea Bartha on 8/8/23.
+//
+import AVFoundation
+import SwiftUI
+
+struct VideoCaptureView: UIViewRepresentable {
+    @Binding var captureSession: AVCaptureSession?
+    @Binding var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        videoPreviewLayer?.removeFromSuperlayer()
+        if let videoPreviewLayer = videoPreviewLayer {
+            uiView.layer.addSublayer(videoPreviewLayer)
+        }
+    }
+}
+
+
